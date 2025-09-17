ea29f915db8506bdd8f575ea653f10a50314983a
YuxinRong1 <2035209@apps.nsd.org>
2025-09-17 16:13:49 +0000

    I am still learning

---
+++ src/main/java/edu/bhscs/Awesome.java
@@ -0,0 +1,15 @@
+package edu.bhscs;
+
+public class Awesome{
+  String name;
+  public Awesome(String name){
+    System.out.println("I am the Awesome" + name);
+      System.out.println("I am Awesome");
+      this.name = name;
+
+
+  }
+  String getblame(){
+    return this.name;
+  }
+}
--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -1,13 +1,23 @@
-// Student name
-// class period
-// project name
-// date
-
-/*
- * DESCRIPTION:
- * INPUT:
- * OUTPUT:
- * EDGE CASE:
- */
-
 package edu.bhscs;
+public class Main {
+  public static final float MAME_OF_THIS_FLOAT = 69.42f;
+
+  public static final char A = 'a';
+  public static void main(String[] args) {
+
+    System.out.println(A);
+    String myString = pushup("Garlic");
+    System.out.println(myString);
+
+    Awesome Steve = new Awesome("Steve");
+    Awesome Sue = new Awesome("Sue");
+    Awesome Linda = new Awesome("L");
+    Awesome Jhon = new Awesome("With a JH,damb-ass");
+
+    System.out.println( Steve.getblame() );
+
+  }
+  public static String pushup(String food){
+    return "sweet" + food;
+  }
+}
