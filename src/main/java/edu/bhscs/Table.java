7b02749281253a9d678c716d3aa6968b2eac3b84
YuxinRong1 <2035209@apps.nsd.org>
2025-11-04 09:51:47 -0800

    6y

--- src/main/java/edu/bhscs/Table.java
+++ src/main/java/edu/bhscs/Table.java
@@ -4,25 +4,47 @@ public class Table {
   // fields and properties
   int legs;
   int width;
+  String leg;
+  String top = "="; // = "i am in the box";
 
   // constructors
   public Table(int legs, int width) {
     this.legs = legs;
     this.width = width;
+    this.leg = "+";
+    this.top = "=";
     // THIS BIT...
     // INSIDE THE CURRLY BRACKETS.... { ... }
     // this is the actual action.... ...aka... how to do stuff.
 
   }
 
+  public void setleg(String leg) {
+    this.leg = leg;
+  }
+
+  public void settop(String top) {
+    this.top = top;
+  }
+
   // methods
   void draw() {
 
-    for (int i = 0; i < this.legs; i++) {
+    for (int i = 0; i < this.width; i++) {
 
+      System.out.print(top);
       /*things it does*/
-      System.out.println("=        =");
+
     }
+    for ( int i = 0; i <this.legs; i++){
+      System.out.println(leg);
+        for (int j = 0; j < this.width-1; j++){
+          System.out.println(" ");
+        }
+        System.out.println(leg);
+
+    }
+    System.out.println();
     System.out.println("The cake is put on a table with " + this.legs + " legs");
   }
 }
